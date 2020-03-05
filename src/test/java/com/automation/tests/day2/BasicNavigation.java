c609fee43414a4c9a4371fd6d05778a090d89283
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-04 23:33:42 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -20,8 +20,15 @@ public class BasicNavigation {
         // method that return page title
         // you can also see it as tab name, in the browser
         String title = driver.getTitle(); // returns <title>Some title</title> text
-        System.out.println("Title is ..." + title);
+        String expectedTitle = "Google";
 
+        System.out.println("Title is ..." + title);
+        if(expectedTitle.equals(title)){
+            System.out.println("TEST PASSED!");
+        }else {
+            System.out.println("TEST FAILED!");
+        }
+        // must be at the end
         driver.close(); // to close browser
         // browser cannot close itself
 
