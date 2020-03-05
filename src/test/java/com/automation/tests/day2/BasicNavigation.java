9e4876733f38c07f045affdd0a3a18b94d377977
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-05 12:20:41 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day2/BasicNavigation.java
+++ src/test/java/com/automation/tests/day2/BasicNavigation.java
@@ -16,6 +16,8 @@ public class BasicNavigation {
    // In selenium, everything starts from WebDriver interface
    // ChromeDriver extends RemoteWebdriver --> implements WebDriver
         driver.get("http://google.com"); // to open a website
+        driver.manage().window().maximize(); // to maximize browser
+       // driver.manage().window().fullscreen();
         Thread.sleep(3000);// for demo wait 3 seconds
         // method that return page title
         // you can also see it as tab name, in the browser
@@ -28,9 +30,39 @@ public class BasicNavigation {
         }else {
             System.out.println("TEST FAILED!");
         }
+// got to another website within the same window
+        driver.navigate().to("http://amazon.com");
+
+        if (driver.getTitle().toLowerCase().contains("amazon")){
+            System.out.println("TEST PASSED!!");
+        }else{
+            System.out.println("TEST FAILED!!");
+        }
+//comeback to google
+ driver.navigate().back();
+
+ // checking if page title is equals to Google
+ // .getTitle()- returns page title
+verifyEquals(driver.getTitle(),"Google");
+
         // must be at the end
         driver.close(); // to close browser
         // browser cannot close itself
-
+/**
+ * Check if to strings are same. If print TEST PASSED! message.
+ * Otherwise, print TEST FAILED message
+ * @param arg1
+ * @param arg2
+*/
     }
+
+    public static void verifyEquals(String arg1,String arg2){
+        if (arg1.equals(arg2)){
+            System.out.println("TEST PASSED!!!");
+        }else {
+            System.out.println("TEST FAILED!!!");
+        }
+    }
+
+
 }
