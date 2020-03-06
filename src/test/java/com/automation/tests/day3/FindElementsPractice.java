3036fa163c27e5400bc5ab1f457cddbbb012ef9a
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-05 23:36:25 -0500

    created basic navigation class

--- src/test/java/com/automation/tests/day3/FindElementsPractice.java
+++ src/test/java/com/automation/tests/day3/FindElementsPractice.java
@@ -1,16 +1,30 @@
 package com.automation.tests.day3;
 
+import com.automation.tests.utilities.DriverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FindElementsPractice {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
 
-        WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+    //    WebDriverManager.chromedriver().setup();
+    //    WebDriver driver = new ChromeDriver();
+WebDriver driver = DriverFactory.createADriver("chrome");
+driver.get("http://practice.cybertekschool.com/sign_up");
+Thread.sleep(4000);
 
+        WebElement fullName = driver.findElement(By.name("full_name"));
+        fullName.sendKeys("Mister Twister");
+WebElement email = driver.findElement(By.name("email"));
+email.sendKeys("sdet@cybertek.com");
+
+WebElement signUp =  driver.findElement(By.name("wooden_spoon"));
+signUp.click();
+        driver.quit(); // to close everything
 
     }
 }
