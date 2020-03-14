fd2bd2b77a59cc2ec21a6d6906dbc8532acac785
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-14 09:13:04 -0400

    created basic navigation class

---
+++ src/test/java/com/automation/tests/utilities/BrowserUtils.java
@@ -0,0 +1,17 @@
+package com.automation.tests.utilities;
+
+public class BrowserUtils {
+    /**
+     * Pause test for some time
+     * @param seconds
+     */
+ public static void wait (int seconds){
+     try{
+         Thread.sleep(1000 * seconds);
+     } catch (InterruptedException e ){
+         e.printStackTrace();
+     }
+
+
+    }
+}
