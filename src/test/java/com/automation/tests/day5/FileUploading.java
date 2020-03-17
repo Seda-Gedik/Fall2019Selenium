261127a56d1ffc0f31ebcbe9f8ad70ec41a653c0
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-17 15:17:03 -0400

    created basic navigation class

---
+++ src/test/java/com/automation/tests/day5/FileUploading.java
@@ -0,0 +1,37 @@
+package com.automation.tests.day5;
+
+import com.automation.tests.utilities.BrowserUtils;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FileUploading {
+    public static void main(String[] args) {
+
+        WebDriverManager.chromedriver().version("79").setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://practice.cybertekschool.com/upload");
+        BrowserUtils.wait(4);
+
+
+        WebElement upload = driver.findElement(By.id("file-upload"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+driver.quit();
+    }
+}
