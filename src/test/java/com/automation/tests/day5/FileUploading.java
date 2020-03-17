ba12feab8f01b83f80ad5f2900caf14ae4157149
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-17 15:48:44 -0400

    created basic navigation class

--- src/test/java/com/automation/tests/day5/FileUploading.java
+++ src/test/java/com/automation/tests/day5/FileUploading.java
@@ -18,16 +18,23 @@ public class FileUploading {
 
         WebElement upload = driver.findElement(By.id("file-upload"));
 
+        // https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
+        // I am gonna upload pom.xml file
+        String filePath = System.getProperty("user.dir")+"/pom.xml";
+        // it work only for my computer because only have this file
+        // and my computer username is different than yours
+        String file2Path = "/Users/sedagedik/Desktop/Rough Draft-Seda.docx";
+//        doesn't work, should be on your computer
+//        String cloudFile = "https://cybertek-appium.s3.amazonaws.com/etsy.apk";
+
+        System.out.println(filePath); // print path
+
+        upload.sendKeys(file2Path);
+
+        driver.findElement(By.id("file-submit")).click(); // click to upload
 
 
-
-
-
-
-
-
-
-
+        BrowserUtils.wait(5);
 
 
 
