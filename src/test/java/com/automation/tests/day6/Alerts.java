80b472cadcacf03e0aa92ce1440d700ec11144de
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-19 17:00:35 -0400

    created basic navigation claSS

--- src/test/java/com/automation/tests/day6/Alerts.java
+++ src/test/java/com/automation/tests/day6/Alerts.java
@@ -2,7 +2,11 @@ package com.automation.tests.day6;
 
 import com.automation.tests.utilities.BrowserUtils;
 import com.automation.tests.utilities.DriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 public class Alerts {
     public static void main(String[] args) {
@@ -10,7 +14,30 @@ public class Alerts {
         driver.get("http://practice.cybertekschool.com/javascript_alerts");
         BrowserUtils.wait(3);
 
+        List<WebElement> buttons = driver.findElements(By.tagName("button"));
 
+        buttons.get(0).click();//to click on the first button
+        BrowserUtils.wait(3);
+        //to get the text from popup message
+        String popupText = driver.switchTo().alert().getText();
+        System.out.println(popupText);
+
+        driver.switchTo().alert().accept();//to click OK
+
+        String expected = "You successfully clicked an alert";
+        String actual = driver.findElement(By.id("result")).getText();
+
+        //if will fail, because there is a typo ##BUG##
+        System.out.println("TEST #1");
+        if(expected.equals(actual)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+            System.out.println("Expected: "+expected);
+            System.out.println("Actual:   "+actual);
+
+        }
+        BrowserUtils.wait(3);
 
 
 
