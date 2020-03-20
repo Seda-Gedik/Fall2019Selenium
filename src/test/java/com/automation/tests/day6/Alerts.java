d60fa41dffcbce266eb8d0fd9ef014ecefb18b76
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-19 21:46:25 -0400

    created basic navigation claSS

--- src/test/java/com/automation/tests/day6/Alerts.java
+++ src/test/java/com/automation/tests/day6/Alerts.java
@@ -2,6 +2,7 @@ package com.automation.tests.day6;
 
 import com.automation.tests.utilities.BrowserUtils;
 import com.automation.tests.utilities.DriverFactory;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -39,6 +40,53 @@ public class Alerts {
         }
         BrowserUtils.wait(3);
 
+//####################################################################################
+        System.out.println("TEST #2");
+
+        buttons.get(1).click();//to click on the 2nd button
+        BrowserUtils.wait(3);
+        //to click cancel
+        driver.switchTo().alert().dismiss(); // result must be: You clicked: Cancel
+
+        String expected2 = "You clicked: Cancel";
+        String actual2 = driver.findElement(By.id("result")).getText();
+
+        if(expected2.equals(actual2)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+            System.out.println("Expected: "+expected2);
+            System.out.println("Actual:   "+actual2);
+        }
+
+//####################################################################################
+        System.out.println("TEST #3");
+
+        //TASK for 5 minutes until 3:37: click on button #3
+        //Enter some text: Hello, World!
+        //Verify that result text ends with Hello, World!
+        buttons.get(2).click();
+
+        BrowserUtils.wait(3);
+
+        Alert alert = driver.switchTo().alert();
+
+        alert.sendKeys("Hello, World!");//enter text
+        BrowserUtils.wait(3);
+
+
+        alert.accept();// click ok
+
+        String actual3 = driver.findElement(By.id("result")).getText();
+        String expected3 = "Hello, World!";
+
+        if(actual3.endsWith(expected3)){
+            System.out.println("TEST PASSED");
+        }else {
+            System.out.println("TEST FAILED");
+            System.out.println("Expected: "+expected3);
+            System.out.println("Actual:   "+actual3);
+        }
 
 
 
