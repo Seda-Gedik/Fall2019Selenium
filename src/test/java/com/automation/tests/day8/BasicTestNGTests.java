9620734ba3f70dcc600022eab94df50ce6c7526b
Seda-Gedik <sedayesilyurt58@gmail.com>
2020-03-23 00:01:53 -0400

    day 8

--- src/test/java/com/automation/tests/day8/BasicTestNGTests.java
+++ src/test/java/com/automation/tests/day8/BasicTestNGTests.java
@@ -1,9 +1,52 @@
 package com.automation.tests.day8;
 
 import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.testng.annotations.*;
 
 public class BasicTestNGTests {
+
+        //runs only once before @BeforeClass and @BeforeMethod
+        @BeforeTest
+        public void beforeTest(){
+            System.out.println("BEFORE TEST");
+        }
+
+        //runs only once after @AfterClass after @AfterMethod
+        @AfterTest
+        public void afterTest(){
+            System.out.println("AFTER TEST");
+        }
+
+        //Runs only once in the class before @beforemethod and before any test
+        //regardless on number of tests, it runs only once
+        @BeforeClass
+        public void beforeClass(){
+            //something that should be done only once in the class before all tests
+            System.out.println("BEFORE CLASS");
+        }
+
+        @AfterClass
+        public void afterClass(){
+            //something that should be done only once in the class after all tests
+            System.out.println("AFTER CLASS");
+        }
+
+        //runs before every test automatically
+        //works as a pre-condition or setup
+        @BeforeMethod
+        public void setup(){
+            System.out.println("BEFORE METHOD");
+        }
+
+        //runs automatically after every test
+        @AfterMethod
+        public void teardown(){
+            System.out.println("AFTER METHOD");
+        }
+
+
+
+
 
     @Test
     public void test1() {
